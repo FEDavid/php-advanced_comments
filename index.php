14992a2d8bade90071d4eeb8ef9e2899b97b4e0e
David <91611885+FEDavid@users.noreply.github.com>
2024-08-22 10:20:12 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Website</title>
+</head>
+<body>
+    <h1>Welcome to my PHP Website</h1>
+
+    <?php
+    echo "Hello World!";
+    ?>
+
+</body>
+</html>
